c8be0aaf4f0d8150f25037deb7db8364d4e0d6af
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-11-15 22:45:19 -0300

    chore: remove all header titles

--- app/(stacks)/_layout.tsx
+++ app/(stacks)/_layout.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 
 export default function StackLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="cultura"
-        options={{
-          headerShown: false,
-          title: 'Cultura Fatec',
-        }}
-      />
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        title: '',
+      }}
+    >
     </Stack>
   );
 }
